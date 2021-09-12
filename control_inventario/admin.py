a65b9765e9b6a9f0fd04292e930ecde0a491acc4
Anonys19 <arsrojas@est.itsgg.edu.ec>
2021-09-11 20:04:11 -0500

    subir

---
+++ control_inventario/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from.models import rol_persona,Persona,ciudad,proveedor
+# Register your models here.
+admin.site.register(rol_persona)
+admin.site.register(Persona)
+admin.site.register(ciudad)
+admin.site.register(proveedor)
